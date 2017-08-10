0cb69653d5d541b0387931186306b65da051c8a3
Mark Helt and Riki Montgomery <riki@>
2017-08-10 14:50:26 -0700

    Attempt to fix if else statements (still broken). Possible {} error on line 37.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,75 @@ $(document).ready(function() {
     var gender = $("select#gender").val();
     var eyeColor = $("select#eyeColor").val();
     var favoriteNumber =  parseInt($("input#favoriteNumber").val());
-    alert("Blah Blah");
 
-    // if (age) {
-    //
-    // } else {
-    //   alert("Please Enter Ager");
-    // }
+    if (age) {
+    } else {
+      alert("Please Enter age!");
+    }
+
+    if (favoriteNumber) {
+    } else {
+      alert("Please enter favorite number!")
+    }
+
+    if (gender === "male"){
+      if (age <50){
+        if (eyeColor === "blue"){
+          // m,<50,bl
+          (".kardashian").show();
+        }else if (eyeColor === "brown"){
+          // m,<50,br
+          (".murray").show();
+        }else {
+          // m,<50,gr
+          (".kardashian").show();
+        }
+      } else if {
+        if (eyeColor === "blue"){
+    //       // m,>50,bl
+          (".murray").show();
+        }else if (eyeColor === "brown"){
+    //       // m,>50,br
+          (".kardashian").show();
+        }else {
+    //       // m,>50,gr
+          (".murray").show();
+        }
+      }
+    } else { //end of (gender === "male")
+      if(age <50){
+        if (eyeColor === "blue"){
+    //       // f,<50,bl
+          (".kardashian").show();
+        }else if (eyeColor === "brown"){
+    //       // f,<50,br
+          (".murray").show();
+        }else {
+    //       // f,<50,gr
+          (".kardashian").show();
+        }
+      } else{
+        if (eyeColor === "blue"){
+    //       // f,>50,bl
+          (".murray").show();
+        }else if (eyeColor === "brown"){
+    //       // f,>50,br
+          (".kardashian").show();
+        }else {
+    //       // f,>50,gr
+          (".murray").show();
+        }
+      }
+// closing
+    }
 
     event.preventDefault();
   });
 });
+
+
+// if (age <= 50 && gender === male && eyeColor === blue || green){
+//   (".kardashian").show();
+//     if (age <= 50 && gender === male && eyeColor === blue || green){
+//     }
+// }
